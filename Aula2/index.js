1ce3588642ac4673acf533939b5098ccfc6f9dbf
Luana-s <luana.silva1@arapiraca.ufal.br>
2022-10-01 18:56:06 -0300

    Manipulando arrays

--- Aula2/index.js
+++ Aula2/index.js
@@ -1,13 +1,63 @@
-const linguagens =['JS',1,2,3,'javaScript','PHP']
+/*const alunas =["Maria","Paula", "Vera"]
 
-console.log(linguagens[0])
+console.log(alunas)
 
-linguagens.push('python')
 
-console.log("Array modificado",linguagens)
+//imprimindo pela posicao
+console.log(alunas[1])
 
-linguagens.pop(2)
-console.log("removendo do array",linguagens)
+
+//adicionando no array
+alunas.push('Nadja')
+
+console.log("Array modificado",alunas)
+
+
+  //removendo do array
+alunas.pop()
+console.log("removendo do array",alunas)
+
+  //tamanho
+console.log(alunas.length)*/
+
+
+
+
+
+/*const notas=[1,2,3]
+//funcao que retorna um elemento do array
+notas.forEach((elemento)=>{
+    console.log(elemento)
+
+})*/
+
+/*const linguagens =['JS',1,2,3,'javaScript','PHP']
+// isNan() funcao que retorna se e numerico
+
+linguagens.forEach((i)=>{
+    if (isNaN(i)){
+        console.log(i)
+    }
+})*/
+const alunas =["Maria","Paula", "Vera"]
+const cursos=["js","html","css"]
+
+
+const pretalab=[alunas,cursos]
+
+const cursoslivres=["minasprogramam",pretalab,"programaria"]
+
+
+//posicao do array e posicao do elemento
+//console.log(pretalab[0][1])
+
+console.log(cursoslivres[1][1][2])
+
+
+
+
+
+
 
 
 
