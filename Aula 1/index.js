9fcb214821af20d4e71e0e692a3cbcda297e127c
Luana-s <luana.silva1@arapiraca.ufal.br>
2022-10-01 18:59:21 -0300

    Funções

--- Aula 1/index.js
+++ Aula 1/index.js
@@ -9,11 +9,11 @@ if(idade >= 18 && idade <70){
 
 }*/
 
-/*function somar(num1 ,num2){
+function somar(num1 ,num2){
     const result=num1 + num2
     console.log(result)
 }
-function bemVinda(nome,idade){
+/*function bemVinda(nome,idade){
     console.log("Bem vinda," +  nome + ". Parabens pelos " + idade + " anos")
 
     }
